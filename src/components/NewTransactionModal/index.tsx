30231a00c826488b686c6b8b42df46463ea8914e
thyago608 <thyagoribeiro608@gmail.com>
2021-09-14 13:43:01 -0400

    Added component TransactionTypeButton

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, TransactionTypeContainer} from './styles';
+import { Container, TransactionTypeContainer, Button } from './styles';
 
 type NewTransactionModalProps = {
     isOpen:boolean;
@@ -10,6 +11,17 @@ type NewTransactionModalProps = {
 };
 
 export function NewTransactionModal({isOpen,onRequestClose }:NewTransactionModalProps){
+    const [category, setCategory]= useState('');
+    const [title, setTitle]= useState('');
+    const [value, setValue]= useState(0);
+    
+    const [type, setType] = useState('deposit');
+    
+    function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault();
+
+    }
+
     return(
         <Modal
             isOpen={true}
@@ -25,7 +37,7 @@ export function NewTransactionModal({isOpen,onRequestClose }:NewTransactionModal
                 <img src={closeImg} alt="Fechar Modal"/>
             </button>
            
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar Transação</h2>
 
                 <input 
@@ -39,15 +51,25 @@ export function NewTransactionModal({isOpen,onRequestClose }:NewTransactionModal
                 />
 
                 <TransactionTypeContainer>
-                    <button type="button">
+                    <Button 
+                        type="button"
+                        onClick={()=> setType('deposit')}
+                        isActive={type === 'deposit'}
+                        color='green'
+                    >
                         <img src={incomeImg} alt="Entrada"/>
                         <span>Entrada</span>
-                    </button>
+                    </Button>
 
-                    <button type="button">
+                    <Button 
+                        type="button"
+                        onClick={()=>{setType('withdraw')}}
+                        isActive={type === 'withdraw'}
+                        color='red'
+                    >
                         <img src={outcomeImg} alt="Saída"/>
                         <span>Saída</span>
-                    </button>
+                    </Button>
                 </TransactionTypeContainer>
                 
                 <input 
