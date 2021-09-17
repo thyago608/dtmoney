c67ab182e4b2db3c91ae0d2053a5d893dd0c01e2
thyago608 <thyagoribeiro608@gmail.com>
2021-09-17 17:52:33 -0400

    Add component CarrouselWrapper

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,9 @@
 import { useTransactions } from '../../hooks/useTransactions';
-import { Container } from "./styles";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
+import { Container, CarrouselWrapper, Carrousel } from "./styles";
 
 export function Summary() {
   const { transactions } = useTransactions();
@@ -26,38 +27,50 @@ export function Summary() {
   });
   return (
     <Container>
-      <section>
-        <header>
-          <p>Entradas</p>
-          <img src={incomeImg} alt="Entradas" />
-        </header>
-        <strong>{new Intl.NumberFormat('pt-BR',{
-          style:'currency',
-          currency:'BRL'
-        }).format(summary.deposits)}</strong>
-      </section>
+      <CarrouselWrapper>
+          <Carrousel>
+            <section>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>{new Intl.NumberFormat('pt-BR',{
+                style:'currency',
+                currency:'BRL'
+                }).format(summary.deposits)}</strong>
+            </section>
 
-      <section>
-        <header>
-          <p>Saídas</p>
-          <img src={outcomeImg} alt="Saídas" />
-        </header>
-        <strong>- {new Intl.NumberFormat('pt-BR',{
-          style:'currency',
-          currency:'BRL'
-        }).format(summary.withdraws)}</strong>
-      </section>
+            <section>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>- {new Intl.NumberFormat('pt-BR',{
+                style:'currency',
+                currency:'BRL'
+                }).format(summary.withdraws)}</strong>
+            </section>
 
-      <section className="highlight-background">
-        <header>
-          <p>Total</p>
-          <img src={totalImg} alt="Total" />
-        </header>
-        <strong>{new Intl.NumberFormat('pt-BR',{
-          style:'currency',
-          currency:'BRL'
-        }).format(summary.total)}</strong>
-      </section>
+            <section className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>{new Intl.NumberFormat('pt-BR',{
+                style:'currency',
+                currency:'BRL'
+                }).format(summary.total)}</strong>
+            </section>
+        </Carrousel>
+      </CarrouselWrapper>
+
+      <button type="button" className="button button-previous">
+          <FiChevronLeft/>
+      </button>
+
+      <button type="button" className="button button-next">
+          <FiChevronRight/>
+      </button>
     </Container>
   );
 }
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,10 +1,55 @@
 import styled from "styled-components";
+import { transparentize} from 'polished';
 
 export const Container = styled.div`
+  position: relative;
+
+  .button{
+    padding: 0 0.1rem;
+    height: 10rem;
+    font-size: 2rem;
+    color: var(--text-title);
+    background: ${transparentize(0.5,'#ccc')};
+    border: 0;
+
+    position: absolute;
+
+    &:hover{
+      filter: brightness(0.9);
+    }
+  }
+
+  .button-previous{
+    top:0;
+    left:0;
+  }
+
+  .button-next{
+    top:0;
+    right: 0px;
+  }
+
+
+  @media(min-width: 720px){
+    .button{
+      display:none;
+    }
+  }
+`;
+
+
+export const CarrouselWrapper = styled.div`
+  margin-top: -10rem;
+
+  overflow-x: hidden;
+`;
+
+export const Carrousel = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
-  margin-top: -10rem;
+
+  transform:translateX(-100px);
 
   section {
     background: var(--shape);
@@ -29,6 +74,24 @@ export const Container = styled.div`
     &.highlight-background {
       background: var(--green);
       color: #fff;
+    }
+
+    @media(max-width:620px){
+      strong{
+        font-size: 1.3rem;
+      }
+    }
+
+
+    @media(max-width:720px){
+      &{
+        width: 28rem;
+        height: 10rem;
+
+        &:first-of-type{
+          padding-left: 2.8rem;
+        }
+      }
     }
   }
 `;
